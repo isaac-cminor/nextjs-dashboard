19345e5f6515cc6f8e437be9db82967371b993de
isaacmwesigwa1 <isaacmwesigwa@safeboda.com>
2024-08-26 12:00:12 +0300

    fourth commit

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -23,11 +23,11 @@ export default async function CardWrapper() {
   } = await fetchCardData();
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
 
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      {/* NOTE: Uncomment this code in Chapter 9 */}
       <Card
         title="Total Customers"
         value={numberOfCustomers}
